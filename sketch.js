908d95d2d9081d3fc989b594bf9dec8b0b382ae1
mariaclaraftms <mariaclara.ap.f.c@gmail.com>
2024-06-19 20:41:59 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas (600, 600);
+     background("black");
+ }
+    
+    function draw() {
+    stroke("blue");
+    fill("red");
+    
+    
+    
+    if (mouseIsPressed) {
+    
+     rect(mousex, mouseY, 20, 35); 
+    }
+    
+ }
